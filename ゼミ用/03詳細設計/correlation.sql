0b4cf9d740a1db167467ea7f84b667a295164079
DESKTOP-CQO1HFG\kyome <1120.hetaria@gmail.com>
2021-01-20 13:26:11 +0900

    主キー、外部キー

--- ゼミ用/03詳細設計/correlation.sql
+++ ゼミ用/03詳細設計/correlation.sql
@@ -1,12 +1,39 @@
 /* ユーザ */
-create table cor_user(user_id TEXT, user_name TEXT, password TEXT, version INTEGER);
+CREATE TABLE cor_user(
+    user_id TEXT NOT NULL, user_name TEXT NOT NULL, password TEXT, version INTEGER NOT NULL,
+    PRIMARY KEY (user_id),
+    );
 /* 登場人物マスタ */
-create table acter(acter_id TEXT, acter_name TEXT, acter_conf TEXT, acter_img TEXT, time_id TEXT, group_id TEXT, version INTEGER);
+CREATE TABLE acter(
+    acter_id TEXT NOT NULL, acter_name TEXT NOT NULL, acter_conf TEXT, acter_img TEXT, time_id TEXT NOT NULL, group_id TEXT NOT NULL, version INTEGER NOT NULL
+    PRIMARY KEY (acter_id),
+    FOREIGN KEY (time_id) REFERENCES time_mst(time_id)
+    FOREIGN KEY (group_id) REFERENCES group_mst(group_id)
+    );
 /* 時系列マスタ */
-create table time_mst(time_id TEXT, time_name TEXT, group_id TEXT, version INTEGER);
+CREATE TABLE time_mst(
+    time_id TEXT NOT NULL, time_name TEXT NOT NULL, group_id TEXT NOT NULL, version INTEGER NOT NULL
+    PRIMARY KEY (time_id),
+    FOREIGN KEY (group_id) REFERENCES group_mst(group_id)
+    );
 /* 関係 */
-create table rel(rel_id TEXT, rel_mst_id TEXT, acter_id TEXT, select_id TEXT, user_id TEXT, version INTEGER);
+CREATE TABLE rel(
+    rel_id TEXT NOT NULL, rel_mst_id TEXT NOT NULL, acter_id TEXT NOT NULL, select_id TEXT NOT NULL, user_id TEXT NOT NULL, version INTEGER NOT NULL
+    PRIMARY KEY (rel_id),
+    FOREIGN KEY (rel_mst_id) REFERENCES rel_mst(rel_mst_id)
+    FOREIGN KEY (acter_id) REFERENCES acter(acter_id)
+    /*FOREIGN KEY (select_id) REFERENCES time_mst(select_id) */
+    FOREIGN KEY (user_id) REFERENCES cor_user(user_id)
+    );
 /* 関係性マスタ */
-create table rel_mst(rel_mst_id TEXT, rel_mst_name TEXT, group_id TEXT, version INTEGER);
+CREATE TABLE rel_mst(
+    rel_mst_id TEXT NOT NULL, rel_mst_name TEXT NOT NULL, group_id TEXT NOT NULL, version INTEGER NOT NULL
+    PRIMARY KEY (rel_mst_id),
+    FOREIGN KEY (group_id) REFERENCES group_mst(group_id)
+    );
 /* グループマスタ */
-create table group_mst(group_id TEXT, group_name TEXT, user_id TEXT, version INTEGER);
+CREATE TABLE group_mst(
+    group_id TEXT NOT NULL, group_name TEXT NOT NULL, user_id TEXT NOT NULL, version INTEGER NOT NULL
+    PRIMARY KEY (group_id)
+    FOREIGN KEY (user_id) REFERENCES cor_user(user_id)
+    );
