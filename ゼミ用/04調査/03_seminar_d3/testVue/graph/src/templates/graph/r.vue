75d164f4e80921f02a93d5618aa2c6174a40cbdd
ryomeblog <ryome.blog@gmail.com>
2021-09-26 00:08:39 +0900

    todoリスト修正、vueプロジェクト名変更

---
+++ ゼミ用/04調査/03_seminar_d3/testVue/graph/src/templates/graph/r.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row">
+    <router-link v-bind:to="{ name: 'test2' }">
+      <b-button variant="primary"> test2</b-button>
+    </router-link>
+    <router-link v-bind:to="{ name: 'graphCreate' }">
+      <b-button variant="primary"> graphCreate</b-button>
+    </router-link>
+    <router-link v-bind:to="{ name: 'graphSubmit' }">
+      <b-button variant="primary"> graphSubmit</b-button>
+    </router-link>
+    <router-link v-bind:to="{ name: 'graphList' }">
+      <b-button variant="primary"> graphList</b-button>
+    </router-link>
+  </div>
+</template>
+
